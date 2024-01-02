d9105c8392caa26c9af22c10e64cf5e20ecdc33f
undefined-dev74 <aman.kumar.jha953@gmail.com>
2024-01-02 12:46:08 +0530

    feat : send token verification email

--- auth.ts
+++ auth.ts
@@ -25,6 +25,7 @@ export const {
   },
   callbacks: {
     async signIn({ user, account }) {
+      console.log({ user, account });
       const existingUser = await getUserById(user.id);
       // Allow OAuth without email verification
       if (account?.provider !== "credentials") return true;
