f5030e7fff53c5285bf1bbf4b8d8caa0c02a6391
undefined-dev74 <aman.kumar.jha953@gmail.com>
2024-01-02 20:44:46 +0530

    feat : Added reset password functionality

--- schema/index.ts
+++ schema/index.ts
@@ -14,3 +14,7 @@ export const RegisterSchema = z.object({
 export const ResetSchema = z.object({
   email: z.string().email({ message: "Email is required" }),
 });
+
+export const NewPasswordSchema = z.object({
+  password: z.string().min(6, { message: "Minimum 6 characters required" }),
+});
