f2644e74c09643476a43be3b55099cb8d911cd48
undefined-dev74 <aman.kumar.jha953@gmail.com>
2024-01-02 23:12:40 +0530

    feat : Enabled two factor authentication mode

--- actions/login.ts
+++ actions/login.ts
@@ -1,9 +1,15 @@
 "use server";
 
 import { signIn } from "@/auth";
+import { getTwoFactorConfirmationByUserId } from "@/data/two-factor-confirmation";
+import { getTwoFactorTokenByEmail } from "@/data/two-factor-token";
 import { getUserByEmail } from "@/data/user";
-import { sendVerificationEmail } from "@/lib/mail";
-import { generateVerificationToken } from "@/lib/tokens";
+import { db } from "@/lib/db";
+import { sendTwoFactorTokenEmail, sendVerificationEmail } from "@/lib/mail";
+import {
+  generateTwoFactorToken,
+  generateVerificationToken,
+} from "@/lib/tokens";
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 import { LoginSchema } from "@/schema";
 import { AuthError } from "next-auth";
@@ -12,12 +18,12 @@ import * as z from "zod";
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   console.log(values);
   const validatedFields = LoginSchema.safeParse(values);
-
+  generateTwoFactorToken;
   if (!validatedFields.success) {
     return { error: "Invalid fields" };
   }
 
-  const { email, password } = validatedFields.data;
+  const { email, password, code } = validatedFields.data;
 
   const existingUser = await getUserByEmail(email);
 
@@ -36,6 +42,41 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
     );
 
     return { success: "Confirmation email has been sent!" };
+  }
+
+  if (existingUser.isTwoFactorEnabled && existingUser.email) {
+    if (code) {
+      // TODO: Verify the code
+      const twoFactorToken = await getTwoFactorTokenByEmail(existingUser.email);
+
+      if (!twoFactorToken) return { error: "Invalid code!" };
+
+      if (twoFactorToken.token !== code) return { error: "Invalid code!" };
+
+      const hasExpired = new Date(twoFactorToken.expires) < new Date();
+
+      if (hasExpired) return { error: "Code expired" };
+
+      await db.twoFactorToken.delete({ where: { id: twoFactorToken.id } });
+
+      const existingConfirmation = await getTwoFactorConfirmationByUserId(
+        existingUser.id
+      );
+
+      if (existingConfirmation) {
+        await db.twoFactorConfirmation.delete({
+          where: { id: existingConfirmation.id },
+        });
+      }
+
+      await db.twoFactorConfirmation.create({
+        data: { userId: existingUser.id },
+      });
+    } else {
+      const twoFactorToken = await generateTwoFactorToken(existingUser.email);
+      await sendTwoFactorTokenEmail(twoFactorToken.email, twoFactorToken.token);
+      return { twoFactor: true };
+    }
   }
 
   try {
--- lib/mail.ts
+++ lib/mail.ts
@@ -23,3 +23,12 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
     html: `<p>Click here <a href="${resetLink}">here</a> to reset password.</p>`,
   });
 };
+
+export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
+  await resend.emails.send({
+    from: `onboarding@resend.dev`,
+    to: email,
+    subject: "2FA Code",
+    html: `<p>Your 2FA code : ${token} </p>`,
+  });
+};
