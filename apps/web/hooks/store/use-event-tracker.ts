1a6618d43ddd00ac0293d1395b20c9f37c04dfd2
Undefined-dev74 <aman.kumar.jha953@gmail.com>
2024-07-07 13:09:07 +0530

    feat : added localStorage custom hook, store hooks for event tracking

---
+++ apps/web/hooks/store/use-event-tracker.ts
@@ -0,0 +1,13 @@
+import { useContext } from "react";
+// mobx store
+import { StoreContext } from "@/context/store-context";
+// types
+import { IEventTrackerStore } from "@/store/event-tracker-store";
+
+export const useEventTracker = (): IEventTrackerStore => {
+  const context = useContext(StoreContext);
+  if (context === undefined)
+    throw new Error("useEventTracker must be used within StoreProvider");
+  return context.eventTracker;
+};
+ 
