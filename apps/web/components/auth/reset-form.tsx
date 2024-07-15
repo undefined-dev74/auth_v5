246a151153d5b859fb199a84f0da81238e3a3c56
Undefined-dev74 <aman.kumar.jha953@gmail.com>
2024-07-15 09:29:06 +0530

    feat : Reset, and new password services - Added route for email reset password - Added route for send reset password link - toast for error and success notification

--- apps/web/components/auth/reset-form.tsx
+++ apps/web/components/auth/reset-form.tsx
@@ -18,29 +18,47 @@ import { useState, useTransition } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import CardWrapper from "./card-wrapper";
+import { AuthService } from "@/services/auth.service";
+import { useRouter } from "next/navigation";
+import useToast from "@/hooks/use-toast";
+
+const authService = new AuthService()
 
 export const ResetForm = () => {
   const [isPending, setTransition] = useTransition();
   const [error, setError] = useState<string | undefined>("");
   const [success, setSuccess] = useState<string | undefined>("");
-
+  const router = useRouter()
   const form = useForm<z.infer<typeof ResetSchema>>({
     resolver: zodResolver(ResetSchema),
     defaultValues: {
       email: "",
     },
   });
+  
+  const {setToastAlert} = useToast()
 
   const onSubmit = (values: z.infer<typeof ResetSchema>) => {
     console.log(values);
     setError("");
     setSuccess("");
-    setTransition(() => {
-      // reset(values).then((data) => {
-      //   setError(data?.error);
-      //   setSuccess(data?.success);
-      // });
-    });
+   authService.sendResetPasswordLink(values).then((res) => {
+     setSuccess(res?.success) 
+     setToastAlert({
+       type: "success",
+       title: "Success!",
+       message: "We have sent a new link to your email.",
+     });
+     router.push("/auth/new-password")
+     setError(res.error)
+   }).catch((err) => {
+     setError(err)
+     setToastAlert({
+       type: "error",
+       title: "Error!",
+       message: err,
+     })
+   })
   };
   return (
     <CardWrapper
