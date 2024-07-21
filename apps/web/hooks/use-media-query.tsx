9820501574e0f5194306ce525d109fba803d72d0
Undefined-dev74 <aman.kumar.jha953@gmail.com>
2024-07-21 23:32:45 +0530

    feat : Added custom hook for media query and responsiveness

---
+++ apps/web/hooks/use-media-query.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { useEffect, useState } from "react";
+
+const useMediaQuery = (query: string): boolean => {
+  const [matches, setMatches] = useState(false);
+
+  useEffect(() => {
+    const media = window.matchMedia(query);
+    if (media.matches !== matches) {
+      setMatches(media.matches);
+    }
+    const listener = () => setMatches(media.matches);
+    media.addListener(listener);
+    return () => media.removeListener(listener);
+  }, [matches, query]);
+
+  return matches;
+};
+
+export default useMediaQuery;
