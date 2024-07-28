4a32f5043672a1de286e91afb2fd809db508efcb
Undefined-dev74 <aman.kumar.jha953@gmail.com>
2024-07-28 22:46:34 +0530

    chore : type assertion and style indenting

--- apps/web/components/card/auth-card.tsx
+++ apps/web/components/card/auth-card.tsx
@@ -1,17 +1,18 @@
-import MainCard from "@/components/card/main-card";
+
 import { ReactNode } from "react";
-import { Card } from "../ui/card";
+import { Card } from "@/components/ui/card";
 
 type AuthCardProps = {
   children?: ReactNode;
+  title: string;
   [key: string]: any; // This replaces the 'other' prop
 };
 
-const AuthCard = ({ children }: { children: ReactNode }) => (
+const AuthCard = ({ children, title }: AuthCardProps) => (
   <Card className="bg-transparent rounded-lg overflow-hidden border-none">
     <div className="px-8 py-6">
       <h2 className="text-2xl font-bold mb-6 text-white text-center">
-        Log in or Sign up
+        {title}
       </h2>
       <div className="space-y-6">{children}</div>
     </div>
