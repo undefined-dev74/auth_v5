a9791282438eeac7837ccb84d663514942055499
Undefined-dev74 <aman.kumar.jha953@gmail.com>
2024-11-23 13:23:59 +0530

    feat : Added logger utils package

--- packages/utils/anotherUtil.ts
+++ packages/utils/anotherUtil.ts
@@ -1,5 +1,5 @@
-import logger from "@example/logger";
+import logger from '@repo/logger'
 
 export default function oneMoreUtil() {
-  logger.info("another bundled util in its own file");
+  logger.info('another bundled util in its own file')
 }
